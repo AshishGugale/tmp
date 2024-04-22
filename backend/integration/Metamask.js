037a3fc6f6bd8cef6c99aa880720c04f36a61b13
AshishGugale <96615701+AshishGugale@users.noreply.github.com>
2024-04-22 22:35:15 +0530

    Delete backend directory

--- backend/integration/Metamask.js
+++
@@ -1,7 +0,0 @@
-import detectEthereumProvider from '@metamask/detect-provider'
-
-const provider = await detectEthereumProvider();
-if (!provider) {
-    console.log("Error: Please connect your metamask!!!");
-}
-console.log(provider);
