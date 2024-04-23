793ba76a2d3cb746bb903296211e561b63b6b2fc
Ashish <ashishgugale73@gmail.com>
2024-04-23 14:30:11 +0530

    CC LAB

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,5 +8,15 @@ export default defineConfig({
     supported: {
       'top-level-await': true //browsers can handle top-level-await features
     },
-  }
+  },
+  preview: {
+    port: 8080,
+    strictPort: true,
+   },
+   server: {
+    port: 8080,
+    strictPort: true,
+    host: true,
+    origin: "http://0.0.0.0:8080",
+   },
 })
